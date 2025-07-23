a9e5f198465bde6f4aff7db5eebe011c29f34278
MERCADO69 <2201101643@student.buksu.edu.ph>
2025-07-23 21:05:04 +0800

    Update Homescreen.js

--- Navigation/screens/Homescreen.js
+++ Navigation/screens/Homescreen.js
@@ -410,7 +410,7 @@ const styles = StyleSheet.create({
   buttonContent: {
     flexDirection: "row",
     alignItems: "center",
-    paddingLeft: 55,
+    paddingLeft: 65,
   },
   submitText: {
     fontSize: 9,
